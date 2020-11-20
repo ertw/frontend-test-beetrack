e7fedb24696431e8c54e94bd3e1032ba9a44a6e2
Erik Williamson <me@erik.tw>
2020-11-20 14:34:44 -0800

    fix markup

--- frontend/src/components/Users.tsx
+++ frontend/src/components/Users.tsx
@@ -13,16 +13,22 @@ const styles: { [key: string]: React.CSSProperties } = {
 export const Users: React.FC<Props> = () => {
   return (
     <table className="users" style={styles.users}>
-      <th className="userNameHeader">Nombre</th>
-      <th className="userDescriptionHeader">Descripción</th>
-      {[
-        {
-          "id": 1,
-          "name": "Francisco",
-          "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-          "photo": "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?h=350&auto=compress&cs=tinysrgb"
-        },
-      ].map(user => (<User {...user} />))}
+      <thead>
+        <tr>
+          <th scope="col" className="userNameHeader">Nombre</th>
+          <th scope="col" className="userDescriptionHeader">Descripción</th>
+        </tr>
+      </thead>
+      <tbody>
+        {[
+          {
+            "id": 1,
+            "name": "Francisco",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "photo": "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?h=350&auto=compress&cs=tinysrgb"
+          },
+        ].map(user => (<User key={user.id} {...user} />))}
+      </tbody>
     </table>
   );
 }
