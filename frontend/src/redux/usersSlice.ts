0a47bea26f756a52b5f6276c7c40c66afec97ae9
Erik Williamson <me@erik.tw>
2020-11-25 01:08:41 -0800

    dockerize

--- frontend/src/redux/usersSlice.ts
+++ frontend/src/redux/usersSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { RootState } from './store'
 
-const BASE_USERS_API = 'http://localhost:3000/api/users'
+const BASE_USERS_API = 'http://localhost:3001/api/users'
 
 interface User {
   name: string
